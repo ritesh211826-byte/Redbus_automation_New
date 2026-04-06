bc233ff6673a8cace082567cef072578541bfc4b
ritesh211826-byte <ritesh211826@gmail.com>
2026-04-06 16:57:14 +0530

    Update driver_factory.py

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 def get_driver():
     options = Options()
-    options.add_argument("--start-maximized")
+    
+    # 🔥 REQUIRED for GitHub Actions (Linux)
+    options.add_argument("--headless=new")   # or "--headless"
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
+    # Optional (but good)
+    options.add_argument("--disable-gpu")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
 
-    driver.get("https://www.redbus.in")
     return driver
